33e9f1e3ee175e98c3e1934e41394f838ca6de3d
Sachin Alam <sachinalam4216@gmail.com>
2023-01-08 10:46:51 +0530

    7 JAN 2023

---
+++ 2D Array/wavePrint.java
@@ -0,0 +1,18 @@
+public class wavePrint {
+
+  // public static int[] wavePrint(int arr[][], int nRows, int mCols) {
+  // int arr[] = new int[100];
+
+  // return arr[];
+  // }
+
+  public static void main(String[] args) {
+
+    int arr[][] = { { 2, 3 }, { 4, 5 }, { 7, 9 } };
+
+    System.out.println(wavePrint(arr, 0, 0));
+
+
+
+  }
+}
